ee5a7fe1a401e15f03faa3f05d96dedb15dcdeeb
theGobindSingh <thesinghgobind@gmail.com>
2024-01-07 01:59:13 +0530

    dynamic video changes

--- modules/home/hero/video.tsx
+++ modules/home/hero/video.tsx
@@ -1,15 +1,7 @@
-import { useEffect, useRef } from "react";
 import { homeHeroContent } from "@modules/home/content";
 import { heroVideoCss } from "@modules/home/hero/styles";
 
 export default function HomeHeroVideo() {
-  const videoRef = useRef<HTMLVideoElement>(null);
   const { posterImg, videoLink } = homeHeroContent;
-  useEffect(() => {
-    const video = videoRef.current;
-    if (video) {
-      video.src = videoLink;
-    }
-  }, [videoLink]);
-  return <video muted loop autoPlay playsInline ref={videoRef} css={heroVideoCss} poster={posterImg}></video>;
+  return <video muted loop autoPlay playsInline src={videoLink} css={heroVideoCss} poster={posterImg}></video>;
 }
