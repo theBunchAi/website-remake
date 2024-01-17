b0573de7d47e4cac4aa0ab6c25c5d5aa16b75d40
theGobindSingh <thesinghgobind@gmail.com>
2024-01-17 16:03:24 +0530

    Update media queries in common.ts

--- styles/common.ts
+++ styles/common.ts
@@ -1,7 +1,7 @@
 const mobileBreakpoint = 900;
 
 export const mediaQuery = {
-  mobile: `@media (width < ${mobileBreakpoint}px) and (orientation: portrait)`,
-  mobileLandscape: `@media (width < ${mobileBreakpoint}px) and (orientation: landscape)`,
-  desktop: `@media (width >= ${mobileBreakpoint}px)`
+  mobile: `@media only screen and (width < ${mobileBreakpoint}px)`,
+  mobileLandscape: `@media screen and (width < ${mobileBreakpoint}px) and (orientation: landscape)`,
+  desktop: `@media only screen and (width >= ${mobileBreakpoint}px)`
 };
