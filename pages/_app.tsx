bd840f9ac47228263281ce0aaf5d9558e841a87a
theGobindSingh <thesinghgobind@gmail.com>
2024-01-22 22:28:28 +0530

    Add Google Tag Manager and Google Analytics scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,33 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
 import { Global } from "@emotion/react";
 import { globalStyles } from "@/styles/global";
 import Footer from "@modules/footer";
 import Header from "@modules/header";
 
+function GtmIframe() {
+  return (
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-MH5RF4FK"
+        height="0"
+        width="0"
+        style={{ visibility: "hidden", display: "none" }}
+      ></iframe>
+    </noscript>
+  );
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <script async src="/assets/scripts/gtm.js" />
+      </Head>
+      <GtmIframe />
+      <Script strategy="afterInteractive" defer async src="https://www.googletagmanager.com/gtag/js?id=G-7L2RMX12CD" />
+      <Script defer id="ga-analaytics" strategy="afterInteractive" src="/assets/scripts/ga-analytics.js" />
       <Header />
       <Component {...pageProps} />
       <Global styles={[globalStyles]} />
