21fd1ad654c69d9294372edad9c158555ae8225f
theGobindSingh <thesinghgobind@gmail.com>
2024-02-01 21:46:23 +0530

    Fix media query syntax in common.ts

--- styles/common.ts
+++ styles/common.ts
@@ -1,7 +1,7 @@
 const mobileBreakpoint = 900;
 
 export const mediaQuery = {
-  mobile: `@media only screen and (width < ${mobileBreakpoint}px)`,
+  mobile: `@media screen and (width < ${mobileBreakpoint}px)`,
   mobileLandscape: `@media screen and (width < ${mobileBreakpoint}px) and (orientation: landscape)`,
-  desktop: `@media only screen and (width >= ${mobileBreakpoint}px)`
+  desktop: `@media screen and (width >= ${mobileBreakpoint}px)`
 };
