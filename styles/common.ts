e85f8af7e8f9c42c69c995544fe4a1714e3acfaf
theGobindSingh <thesinghgobind@gmail.com>
2024-02-01 22:07:50 +0530

    Update mobile breakpoint and media queries

--- styles/common.ts
+++ styles/common.ts
@@ -1,7 +1,7 @@
-const mobileBreakpoint = 1000;
+const mobileBreakpoint = 1125;
 
 export const mediaQuery = {
-  mobile: `@media only screen and (width < ${mobileBreakpoint}px)`,
+  mobile: `@media only screen and (width <= ${mobileBreakpoint}px)`,
   mobileLandscape: `@media screen and (width < ${mobileBreakpoint}px) and (orientation: landscape)`,
-  desktop: `@media only screen and (width >= ${mobileBreakpoint}px)`
+  desktop: `@media only screen and (width > ${mobileBreakpoint}px)`
 };
